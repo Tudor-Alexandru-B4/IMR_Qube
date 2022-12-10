c5e70cbf37963c86aecae4e3122641ce1b943538
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-12-10 17:47:14 +0200

    Engine Refactoring Done plus Outline Effect on Selected

--- Qube/Assets/Scripts/Engine/Engine.cs
+++ Qube/Assets/Scripts/Engine/Engine.cs
@@ -2,12 +2,15 @@ using System.Collections.Generic;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using cakeslice;
 
 public class Engine : MonoBehaviour
 {
     [SerializeField]
     ARRaycastManager raycastManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    RaycastHit hit;
+    Ray ray;
 
     new Camera camera;
     GameObject selectedObject;
@@ -29,104 +32,131 @@ public class Engine : MonoBehaviour
     bool scalable;
     bool rotatable;
 
-    // Start is called before the first frame update
     void Start()
     {
         selectedObject = null;
         camera = GameObject.Find("AR Camera").GetComponent<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.touchCount == 0)
             return;
 
-        RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.GetTouch(0).position);
+        ray = camera.ScreenPointToRay(Input.GetTouch(0).position);
 
         if (raycastManager.Raycast(Input.GetTouch(0).position, hits))
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began && selectedObject == null)
+            InstantiateHitObject();
+
+            ExecuteIfMovingAMovable();
+            ExecuteIfScalingAScalable();
+            ExecuteIfRotatingARotatable();
+
+            ResetWhenInputEnds();
+        }
+    }
+
+    private void InstantiateHitObject()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began && selectedObject == null)
+        {
+            if (Physics.Raycast(ray, out hit))
             {
-                if (Physics.Raycast(ray, out hit))
+                selectedObject = hit.collider.gameObject;
+                if (selectedObject != null)
                 {
-                    selectedObject = hit.collider.gameObject;
-                    if(selectedObject != null)
+                    string objectTag = selectedObject.tag;
+                    if (canDoAll.Contains(selectedObject.tag))
                     {
-                        string objectTag = selectedObject.tag;
-                        if (canDoAll.Contains(selectedObject.tag))
+                        movable = scalable = rotatable = true;
+                    }
+                    else
+                    {
+                        movable = canMove.Contains(objectTag);
+                        scalable = canScale.Contains(objectTag);
+                        rotatable = canRotate.Contains(objectTag);
+                    }
+
+                    if(movable || scalable || rotatable)
+                    {
+                        Outline outline = selectedObject.GetComponent<Outline>();
+                        if(outline != null)
                         {
-                            movable = scalable = rotatable = true;
+                            outline.eraseRenderer = false;
                         }
                         else
                         {
-                            movable = canMove.Contains(objectTag);
-                            scalable = canScale.Contains(objectTag);
-                            rotatable = canRotate.Contains(objectTag);
+                            selectedObject.AddComponent<Outline>();
                         }
                     }
                 }
             }
-            else if (movable && Input.GetTouch(0).phase == TouchPhase.Moved && selectedObject != null && Input.touchCount == 1)
+        }
+    }
+
+    private void ExecuteIfMovingAMovable()
+    {
+        if (movable && Input.GetTouch(0).phase == TouchPhase.Moved && selectedObject != null && Input.touchCount == 1)
+        {
+            selectedObject.transform.position = hits[0].pose.position;
+        }
+    }
+
+    private void ExecuteIfScalingAScalable()
+    {
+        if (selectedObject != null && scalable && Input.touchCount == 2)
+        {
+            var touchZero = Input.GetTouch(0);
+            var touchOne = Input.GetTouch(1);
+
+            if (touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled || touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
             {
-                selectedObject.transform.position = hits[0].pose.position;
+                return;
             }
 
-            if (selectedObject != null && scalable && Input.touchCount == 2)
+            if (!scaling && selectedObject != null)
             {
-                var touchZero = Input.GetTouch(0);
-                var touchOne = Input.GetTouch(1);
+                scaling = true;
+                initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                initialScale = selectedObject.transform.localScale;
+            }
+            else
+            {
+                var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
 
-                if (touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled || touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
+                if (Mathf.Approximately(initialDistance, 0))
                 {
-                    goto lable_continue;
+                    return;
                 }
 
-                if (!scaling && selectedObject != null)
-                {
-                    scaling = true;
-                    initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
-                    initialScale = selectedObject.transform.localScale;
-                }
-                else
-                {
-                    var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
-
-                    if (Mathf.Approximately(initialDistance, 0))
-                    {
-                        goto lable_continue;
-                    }
-
-                    var factor = currentDistance / initialDistance;
-                    selectedObject.transform.localScale = initialScale * factor;
-
-                }
-
+                var factor = currentDistance / initialDistance;
+                selectedObject.transform.localScale = initialScale * factor;
             }
+        }
+    }
 
-            if (selectedObject != null && rotatable && Input.touchCount == 3)
-            {
-                //selectedObject.transform.LookAt(new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z));
-                selectedObject.transform.rotation = camera.transform.rotation;
-            }
+    private void ExecuteIfRotatingARotatable()
+    {
+        if (selectedObject != null && rotatable && Input.touchCount == 3)
+        {
+            selectedObject.transform.rotation = camera.transform.rotation;
+        }
+    }
 
-        lable_continue:
-
-            if (Input.touchCount != 2 || Input.GetTouch(1).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Canceled)
-            {
-                scaling = false;
-            }
-
-            if (Input.touchCount < 1 || Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
-            {
-                selectedObject = null;
-                movable = scalable = rotatable = false;
-            }
-
+    private void ResetWhenInputEnds()
+    {
+        if (Input.touchCount != 2 || Input.GetTouch(1).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Canceled)
+        {
+            scaling = false;
         }
 
-
+        if (Input.touchCount < 1 || Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+        {
+            selectedObject.GetComponent<Outline>().eraseRenderer = true;
+            selectedObject = null;
+            movable = scalable = rotatable = false;
+        }
     }
 
     public void TriggerLevelSolved(string text = null)
