7765fbc36a5bcc9fa2f09fcabb5d5798e282138b
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-12-16 13:03:08 +0200

    kinda done lvl2

--- Qube/Assets/Scripts/Levels/Level2/LettersLevelIV.cs
+++ Qube/Assets/Scripts/Levels/Level2/LettersLevelIV.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LettersLevelIV : MonoBehaviour
@@ -8,32 +6,17 @@ public class LettersLevelIV : MonoBehaviour
     private Engine engine;
     
     public GameObject myLetter;
-    private GameObject myPlane;
-    private int values;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        myPlane = GetComponent<GameObject>();
-        values = 6;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
 
     void OnTriggerEnter(Collider collider){
         if(collider.tag == "Letter"){
             if(collider.gameObject == myLetter){
-                Destroy(myPlane);
-                values = values-1;
+                engine.ForceReset();
+                collider.transform.position = gameObject.transform.position;
+                collider.transform.rotation = gameObject.transform.rotation;
+                collider.transform.localScale = gameObject.transform.localScale;
                 collider.tag = "Untagged";
+                Destroy(gameObject);
             }
         }
-        if(values == 0){
-            engine.TriggerLevelSolved();
-        } 
     }
 }
